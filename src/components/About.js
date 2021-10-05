6e7f33791170ef29e72a397d495c32bc31872c69
uiukis <wilker.sou26@gmail.com>
2021-10-05 14:14:18 -0300

    version-0.2.1

--- src/components/About.js
+++ src/components/About.js
@@ -66,7 +66,7 @@ export default function About() {
 
                 </div>
 
-                <div className="img-wrapper lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <div className="img wrapper lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                     <img
                         className="object-cover object-center rounded"
                         alt="hero"
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -6,11 +6,7 @@ export default function Testimonials() {
     return (
         <section id="testimonials">
             <div className="banner container px-5 py-10 mx-auto text-center">
-                <div><br></br>
-                    <br></br>
-                    <br></br>
-                </div>
-                <UsersIcon className="w-10 inline-block mb-4" />
+                <UsersIcon className="wrapper w-10 inline-block mb-4" />
                 <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
                     Client Testimonials
                 </h1>
